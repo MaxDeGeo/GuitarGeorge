2cc2013c9c64ba9a4dbca894700a0ccedaa32bbb
Max De George <mtd3442@rit.edu>
2022-01-15 23:30:18 -0500

    initial commit of Guitar George Bot

---
+++ main.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, getVoiceConnection } = require('@discordjs/voice');
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"]});
+
+let sultansResource = createAudioResource('./audio/Sultans.mp3');
+let audioPlayer = createAudioPlayer();
+let connection = null;
+let voiceChannel = null;
+let subscription = null;
+let playerStatus = null;
+
+client.on('ready', () => {
+    console.log('SoSBot is online');
+});
+
+//#region life cycle methods
+// audioPlayer.on(AudioPlayerStatus.Playing, () => {
+//     console.log("Audio Player is Playing");
+// });
+
+// audioPlayer.on(AudioPlayerStatus.Paused, () => {
+//     console.log("Audio Player is Paused");
+// });
+
+// audioPlayer.on(AudioPlayerStatus.Idle, () => {
+//     console.log("Audio Player is Idling");
+// });
+
+// audioPlayer.on('error', error => {
+//     console.log('Error: ', error.message, ' with track ', error.resource.metadata.title);
+// })
+//#endregion
+
+client.on('messageCreate', (message) => {
+    switch (message.content) {
+        case COMMAND.PLAY:
+            const textChannel = client.channels.cache.get(message.channelId);
+            voiceChannel = message.member.voice.channel; 
+
+            if (!connection) {
+                connection = joinVoiceChannel({
+                    channelId: voiceChannel.id,
+                    guildId: voiceChannel.guild.id,
+                    adapterCreator: voiceChannel.guild.voiceAdapterCreator
+                });
+
+                subscription = connection.subscribe(audioPlayer);
+            }
+
+            if (playerStatus === "paused") {
+                audioPlayer.unpause();
+            } else {
+                sultansResource = createAudioResource('./audio/Sultans.mp3');
+                
+                if (playerStatus !== "playing") {
+                    audioPlayer.play(sultansResource);
+                    textChannel.send('Playing Sultans of Swing for Jordan');
+                }
+            }
+            playerStatus = "playing";
+            break;
+        case COMMAND.STOP:
+            audioPlayer.stop();
+            playerStatus = "stopped";
+            break;
+        case COMMAND.PAUSE:
+            audioPlayer.pause();
+            playerStatus = "paused";
+            break;
+        case COMMAND.CONTINUE:
+            audioPlayer.unpause();
+            playerStatus = "playing";
+            break;
+        case COMMAND.LEAVE:
+            getVoiceConnection(voiceChannel.guild.id).destroy();
+            connection = null;
+            playerStatus = "stopped";
+            break;
+        default:
+            break;
+    }
+});
+
+const COMMAND = {
+    PLAY: '/play',
+    STOP: '/stop',
+    PAUSE: '/pause',
+    CONTINUE: '/continue',
+    LEAVE: '/leave',
+}
+
+client.login(process.env.DISCORD_TOKEN);
